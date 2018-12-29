017e8952cd48d072c796a0d986578d613d9e7c81
knancia <oliveira.augusto@outlook.com>
2018-12-29 19:33:34 -0200

    Criação das Entidades User e UserSocial

--- app/Entities/UserSocial.php
+++ app/Entities/UserSocial.php
@@ -2,12 +2,11 @@
 
 namespace App\Entities;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Contracts\Auth\MustVerifyEmail; //Não aparece na video aula
-use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class UserSocial extends Model
 {
     use SoftDeletes; //Aparece somente na video aula
     use Notifiable;
@@ -18,23 +17,13 @@ class User extends Authenticatable
      * @var array
      */
     public      $timestamps     = true;
-    protected   $table          = 'users';
-    protected   $fillable       = ['cpf', 'name', 'phone', 'birth', 'gender', 'notes', 'email', 'password', 'status', 'permission'];
+    protected   $table          = 'user_socials';
+    protected   $fillable       = ['user_id', 'social_network', 'social_id', 'social_email', 'social_avatar',];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected   $hidden         = ['password', 'remember_token',];
+    protected   $hidden         = [];
 }
-
-
-
-
-
-
-
-
-
-
