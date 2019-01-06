f5cd63b3c7d8b00f10362cbfea176e97157ff9f0
knancia <oliveira.augusto@outlook.com>
2019-01-06 03:01:23 -0200

    Implementando mascaras - Aula 021

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -32,6 +32,52 @@ class User extends Authenticatable
     {
         $this->attributes['password'] = env('PASSWORD_HASH') ? bcrypt($value) : $value;
     }
+
+    /**
+     * Mascara CPF para VIEW
+     */
+    public function getCpfAttribute()
+    {
+        $cpf = $this->attributes['cpf'];
+        return substr($cpf, 0, 3). "." .substr($cpf, 3, 3). "." .substr($cpf, 7, 3). "-" .substr($cpf, -2);
+    }
+
+    /**
+     * Mascara Telefone para VIEW
+     */
+    public function getPhoneAttribute()
+    {
+        $phone = $this->attributes['phone'];
+
+        if (strlen($phone) == 10)
+        {
+            return "(" .substr($phone, 0, 2). ") " .substr($phone, 2, 4). "-" .substr($phone, 6, 4);
+        }
+        elseif (strlen($phone) > 10)
+        {
+            return "(" .substr($phone, 0, 2). ") " .substr($phone, 2, 5). "-" .substr($phone, 7, 4);
+        }
+        else
+        {
+            return "Não Informado";
+        }
+    }
+
+    /**
+     * Mascara Data de Nascimento para a VIEW
+     */
+    public function getBirthAttribute()
+    {
+        $birth = explode("-", $this->attributes['birth']);
+        if (count($birth) == 3)
+        {
+            return $birth = $birth[2]. "/" .$birth[1]. "/" .$birth[0];
+        }
+        else
+        {
+            return "Não Informado";
+        }
+    }
 }
 
 
