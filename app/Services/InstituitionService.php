c55d2c810c0cd944821de7e6e03a9519f4a35be9
knancia <oliveira.augusto@outlook.com>
2019-01-10 13:04:31 -0200

    Persistência de dados com Laravel e L5-Repository - Aula 024

---
+++ app/Services/InstituitionService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\InstituitionRepository;
+use App\Validators\InstituitionValidator;
+use Prettus\Validator\Contracts\ValidatorInterface;
+
+class InstituitionService
+{
+    private $repository;
+    private $validator;
+
+    public function __construct(InstituitionRepository $repository, InstituitionValidator $validator)
+    {
+        $this->repository = $repository;
+        $this->validator = $validator;
+
+    }
+
+    public function store(array $data)
+    {
+        $this->validator->with($data)->passesOrFail(ValidatorInterface::RULE_CREATE);
+        $instituition = $this->repository->create($data);
+
+        try 
+        {
+            return [
+                'success'   => true,
+                'messages'  => 'Instituição Cadastrada',
+                'data'      => $instituition,
+            ];
+        }
+        catch (\Exception $e) 
+        {
+            return[
+                'success'   => false,
+                'messages'  => $e->getMessage(),
+            ];
+        }
+    }
+}
